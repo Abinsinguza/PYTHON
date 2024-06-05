75e56b2d177a191a0f7473de7a27ee94cffdd2f5
Abinsinguza <micheallasse96@gmail.com>
2024-06-05 17:37:54 +0300

    Adding lectures

---
+++ Lectures/file.py
@@ -0,0 +1,11 @@
+# RECESS BSSE II Morning session 5th June 2024
+
+
+name = input("what is your name ?")
+fav_color = input("what is your favourate color")
+
+print(name + " likes " + fav_color)
+
+
+#jeff.geoff.phd@gmail.com
+
