1037c7f8d96df8bebf066dee85b7f8085b72bf52
Abinsinguza <micheallasse96@gmail.com>
2024-06-05 17:48:47 +0300

    adding changes

--- Lectures/file.py
+++ Lectures/file.py
@@ -1,9 +1,16 @@
 # RECESS BSSE II Morning session 5th June 2024
 
 
-name = input("what is your name ?")
-fav_color = input("what is your favourate color")
 
+
+# Ask for the user's name
+name = input("what is your name ?" "\n")
+
+# Ask for the user's favorite color
+fav_color = input("what is your favourate color" "\n")
+
+
+# Output the result 
 print(name + " likes " + fav_color)
 
 
