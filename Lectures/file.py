7c0bca9a4ac6d6fd81e23efc4254c7093e71be15
Abinsinguza <micheallasse96@gmail.com>
2024-06-05 18:03:33 +0300

    adding cal

--- Lectures/file.py
+++ Lectures/file.py
@@ -1,8 +1,6 @@
 # RECESS BSSE II Morning session 5th June 2024
 
 
-
-
 # Ask for the user's name
 name = input("what is your name ?" "\n")
 
@@ -12,6 +10,30 @@ fav_color = input("what is your favourate color" "\n")
 
 # Output the result 
 print(name + " likes " + fav_color)
+
+ # Simple addition program
+
+ 
+# Ask the user for the first number
+num1 = input("Enter the first number: ")
+
+
+# Convert the input to an integer
+num1 = int(num1)
+
+
+# Ask the user for the second number
+num2 = input("Enter the second number: ")
+
+# Convert the input to an integer
+num2 = int(num2)
+
+# Calculate the sum
+sum_result = num1 + num2
+
+# Print the result
+print(f"The sum of {num1}and {num2} is {sum_result}.")
+
 
 
 #jeff.geoff.phd@gmail.com
